8a4ff797bdb29553e309d7c950aee1daddebb5fd
Huigen <huigenchen1@gmail.com>
2026-05-01 16:24:33 +0200

    Ejercicio 2

---
+++ Ejercicio Simulacro/Caja.py
@@ -0,0 +1,11 @@
+# palabra = input("Introduzca un texto: ").split()
+# longitud = len(palabra)
+
+# linea = "-" * (longitud + 4)
+# print(linea)
+
+# for p in palabra:
+#     print(f"| {p.ljust(longitud)} |")
+#     print(linea)
+
+# Ejercicios dos
