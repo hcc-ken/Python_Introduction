ecbc17f743cfdb7cacb1c92f5043932c325ebef5
Huigen <huigenchen1@gmail.com>
2026-05-02 12:31:45 +0200

    ejercicio 3

--- Ejercicio Simulacro/Caja.py
+++ Ejercicio Simulacro/Caja.py
@@ -9,3 +9,18 @@
 #     print(linea)
 
 # Ejercicios dos
+
+try:
+    numero = -1
+    while numero != 0:
+        numero = int(input("Introduce un número: "))
+        if numero < 0:
+            print("El número no puede ser negativo") #raise ValueError
+            continue
+
+        suma = 0
+        for i in range(1, numero + 1):
+            suma += i
+        print(suma)
+except ValueError:
+    print("El número no puede ser negativo.")
