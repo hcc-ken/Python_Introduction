b32d2ea59c5a5bf6188eb104c43841c41b1eced5
Huigen <huigenchen1@gmail.com>
2026-05-04 09:26:58 +0200

    último ejercicio

--- Ejercicio Simulacro/Caja.py
+++ Ejercicio Simulacro/Caja.py
@@ -10,17 +10,28 @@
 
 # Ejercicios dos
 
-try:
-    numero = -1
-    while numero != 0:
-        numero = int(input("Introduce un número: "))
-        if numero < 0:
-            print("El número no puede ser negativo") #raise ValueError
-            continue
+# try:
+#     numero = -1
+#     while numero != 0:
+#         numero = int(input("Introduce un número: "))
+#         if numero < 0:
+#             print("El número no puede ser negativo")
+#             continue
 
-        suma = 0
-        for i in range(1, numero + 1):
-            suma += i
-        print(suma)
-except ValueError:
-    print("El número no puede ser negativo.")
+#         suma = 0
+#         for i in range(1, numero + 1):
+#             suma += i
+#         print(suma)
+# except ValueError:
+#     print("El número no puede ser negativo.")
+
+palabra = input("Introiduzca una palabra: ")
+invertida = ""
+
+for letra in palabra:
+    invertida = letra + invertida
+
+if palabra == invertida:
+    print("Es palíndromo")
+else:
+    print("No es palíndromo.")
